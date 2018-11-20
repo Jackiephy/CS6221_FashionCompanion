dca17dac02ef6e8d562e26754f2e68a052231110
Zhangluying <834236513@qq.com>
2018-11-20 11:40:31 -0500

    Add files via upload

---
+++ CS6221_Project_Frame/src/main/java/org/fashion/main/domain/Clothes.java
@@ -0,0 +1,52 @@
+package org.fashion.main.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity @Table(name="CLOTHES")
+public class Clothes implements Serializable{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4567617949982320608L;
+	
+	/** CLOTHES_ID LONG */
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="CLOTHES_ID")
+	private Long clothes_id;
+	
+	/** TYPE VARCHAR2(200) */
+	@Column(name="TYPE",length=200)
+	private String type;
+	
+	
+	/** TAG LONG */
+	@Column(name="TAG")
+	private Long tag;
+	
+	/** PART SHORT */
+	@Column(name="PART")
+	private Short part;
+	
+	/** PRICE DOUBLE */
+	@Column(name="PRICE")
+	private Double price;
+	
+	/** PICTURE VARCHAR2(200) */
+	@Column(name="PICTURE",length=200)
+	private String picture;
+	
+	/** STYLE VARCHAR2(200) */
+	@Column(name="STYLE",length=200)
+	private String style;
+	
+	
+	
+	
+	
+	
+
+}
---
+++ CS6221_Project_Frame/src/main/java/org/fashion/main/domain/User.java
@@ -0,0 +1,44 @@
+package org.fashion.main.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+
+@Entity @Table(name="USER")
+public class User implements Serializable{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8798361091356532548L;
+	
+	
+	/** UNAME VARCHAR2(200) */
+	@Id @Column(name="UNAME",length=200)
+	private String uname;
+	
+	
+	/** PASSWORD VARCHAR2(200) */
+	@Column(name="PASSWORD",length=200)
+	private String passord;
+	
+	
+	/** PREFERENCE VARCHAR2(200) FK(USER_PREFERENCE) */
+	@ManyToMany(fetch=FetchType.LAZY,targetEntity=Clothes.class)
+	@JoinColumn(name="PREFERENCE",referencedColumnName="STYLE",
+				foreignKey=@ForeignKey(name="FK_USER_PREFERENCE"))
+	private String preference;
+	
+	
+	/** ADDRESS VARCHAR2(200)*/
+	@Column(name="ADDRESS",length=200)
+	private String address;
+	
+	/** PHONE VARCHAR2(200)*/
+	@Column(name="PHONE",length=200)
+	private String phone;
+	
+
+}

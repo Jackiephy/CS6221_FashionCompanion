80010c183f1c06c463a5b763c75222fbcbc83034
Jackie Pan <32806229+Jackiephy@users.noreply.github.com>
2018-11-21 00:30:30 +0800

    Add files via upload

---
+++ CS6221_Project_Frame/src/main/java/org/fashion/Cs6221ProjectFrameApplication.java
@@ -0,0 +1,12 @@
+package org.fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs6221ProjectFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs6221ProjectFrameApplication.class, args);
+	}
+}
---
+++ CS6221_Project_Frame/src/test/java/org/fashion/Cs6221ProjectFrameApplicationTests.java
@@ -0,0 +1,16 @@
+package org.fashion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cs6221ProjectFrameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
